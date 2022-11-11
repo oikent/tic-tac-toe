e4bde3c001fd47bf15ea0f01c32610937df3c8d0
oikent <christianstickel@hotmail.com>
2022-11-11 23:46:53 +1100

    adding logic

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 const cells = document.querySelectorAll(".cell")
+const board = document.querySelector('.board')
 
-const cellsArray = Array.from(cells)
+
+
+
+let currentPlayer = 'x'
+
+// board.classList.add(currentPlayer)
+
+
 
 cells.forEach((cell)=>{
     cell.addEventListener('click',()=>{
-        cell.textContent = "x";
+        if(currentPlayer === 'x'){
+            board.classList.add(currentPlayer)
+            cell.classList.add('x')
+            board.classList.remove(currentPlayer)
+            currentPlayer = 'o'
+            
+        }
+        else if(currentPlayer === 'o') {
+            board.classList.add(currentPlayer)
+            cell.classList.add('o')
+            board.classList.remove(currentPlayer)
+            currentPlayer = 'x'
+            
+        }
+
+        
+   
+        
+
+        
+
 })})
 
-// for(cell in cellsArray) {
-//     cell.addEventListener('click',()=>{
-//         cell.textContent = "x";
-//     })
-// }
+
+// Start game with board as x
+// if x clicks cell and cell is empty mark cell as x
+// switch board to o
+// o clicks cell withought class x or o mark cell as o
+// switch statment to check if there are 3 of the same in a row column or diagonal player wins
+// if not draw
+// display winner screen
+// on button restart clear all classes restart game
+
+
