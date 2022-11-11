ad1573fde40eed6044fdd1254275b8c34cd2146f
oikent <christianstickel@hotmail.com>
2022-11-12 03:00:14 +1100

    more logic

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
 const cells = document.querySelectorAll(".cell")
 const board = document.querySelector('.board')
 
+const cellsArray = Array.from(cells)
 
+const winningArray = [
+    [cellsArray[0],cellsArray[1],cellsArray[2]],
+    [cellsArray[3],cellsArray[4],cellsArray[5]],
+    [cellsArray[6],cellsArray[7],cellsArray[8]],
+    [cellsArray[0],cellsArray[3],cellsArray[6]],
+    [cellsArray[1],cellsArray[4],cellsArray[7]],
+    [cellsArray[2],cellsArray[5],cellsArray[8]],
+    [cellsArray[0],cellsArray[4],cellsArray[8]],
+    [cellsArray[2],cellsArray[4],cellsArray[6]]
+]
+console.log(winningArray[0])
+
+for(win of winningArray){
+    // console.log(win[0])
+    
+    for(winner of win){
+        // console.log(winner)
+    }
+    
+}
 
 
 let currentPlayer = 'x'
 
-// board.classList.add(currentPlayer)
-
-
+board.classList.add(currentPlayer)
 
 cells.forEach((cell)=>{
     cell.addEventListener('click',()=>{
-        if(currentPlayer === 'x'){
-            board.classList.add(currentPlayer)
-            cell.classList.add('x')
+        if(currentPlayer === 'x' && !cell.classList.contains('x') && !cell.classList.contains('o') ){
             board.classList.remove(currentPlayer)
             currentPlayer = 'o'
+            board.classList.add(currentPlayer)
+            cell.classList.add('x')
+            
+            
             
         }
-        else if(currentPlayer === 'o') {
-            board.classList.add(currentPlayer)
-            cell.classList.add('o')
+        else if(currentPlayer === 'o' && !cell.classList.contains('x') && !cell.classList.contains('o') ) {
             board.classList.remove(currentPlayer)
             currentPlayer = 'x'
+            board.classList.add(currentPlayer)
+            cell.classList.add('o')
             
-        }
-
-        
-   
-        
-
-        
+        }   
 
 })})
+
+
 
 
 // Start game with board as x
